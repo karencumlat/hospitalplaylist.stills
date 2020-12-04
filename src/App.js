a75c252160e619013c7a6b5680157160ec76a37d
karencumlat <karencumlat@gmail.com>
2020-12-03 23:11:13 -0800

    initial setup for expanding cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import { lackingFive, episode } from './helpers/data';
 import './App.css';
 
 function App() {
+  const [gyosu, setGyosu] = React.useState(1);
+  const [episodeNumber, setEpisodeNumber] = React.useState(13);
+  console.log(episodeNumber);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <span className="tagline">
+          <h1>Hospital Playlist Stills</h1>
+          <span className="tagline--credit">
+            a <a href="https://karencumlat.ca">Karen Cumlat</a> project
+          </span>
+        </span>
+
+        <nav>
+          {episode.map((epi, index) => {
+            const currIndex = index;
+            console.log('curr', currIndex);
+            return (
+              <button
+                key={epi.id}
+                className={
+                  currIndex === episodeNumber - 1
+                    ? 'nav-button active'
+                    : 'nav-button'
+                }
+                onClick={() => setEpisodeNumber(epi.id)}
+              >
+                E{epi.id}
+              </button>
+            );
+          })}
+        </nav>
       </header>
+      <main>
+        {lackingFive.map((five, index) => {
+          const episodeNo = episodeNumber;
+          const imgUrl = `/images/${episodeNo}-${five.tag}.jpg`;
+
+          return (
+            <div
+              key={five.tag}
+              className={index === gyosu ? 'panel active' : 'panel'}
+              style={{ backgroundImage: `url(${imgUrl})` }}
+              onClick={() => setGyosu(lackingFive.indexOf(five))}
+            >
+              <h3>{five.name}</h3>
+            </div>
+          );
+        })}
+      </main>
     </div>
   );
 }
---
+++ src/helpers/data.js
@@ -0,0 +1,77 @@
+export const lackingFive = [
+  {
+    tag: 'jeongwon',
+    name: 'Ahn Jeong-won',
+  },
+  {
+    tag: 'junwan',
+    name: 'Kim Jun-wan',
+  },
+  {
+    tag: 'songhwa',
+    name: 'Chae Song-hwa',
+  },
+  {
+    tag: 'ikjoon',
+    name: 'Lee Ik-joon',
+  },
+  {
+    tag: 'seokhyeong',
+    name: 'Yang Seok-hyeong',
+  },
+];
+
+export const episode = [
+  {
+    id: '01',
+    title: 'Lonely Night',
+  },
+  {
+    id: '02',
+    title: 'Introduce Me a Good Person',
+  },
+  {
+    id: '03',
+    title: 'Aloha',
+  },
+  {
+    id: '04',
+    title: 'Confession is Not Flashy',
+  },
+  {
+    id: '05',
+    title: 'Beautiful My Love',
+  },
+  {
+    id: '06',
+    title: 'In Front of City Hall at the Subway Station',
+  },
+  {
+    id: '07',
+    title: 'You Always',
+  },
+  {
+    id: '08',
+    title: 'With My Tears',
+  },
+  {
+    id: '09',
+    title: 'The Wind Is Blowing',
+  },
+  {
+    id: '10',
+    title: 'Oh! What a Shiny Night',
+  },
+  {
+    id: '11',
+    title: 'I Knew I Love',
+  },
+  {
+    id: '12',
+    title: 'Me to You, You to Me',
+  },
+  {
+    id: '13',
+    title: 'Concert',
+  },
+];
